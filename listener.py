e674019c4b0917149585737bed9e5383ac811543
Ubuntu <ubuntu@ip-172-26-13-238.us-west-2.compute.internal>
2023-10-08 17:37:57 +0000

    listener credentials

--- listener.py
+++ listener.py
@@ -1,4 +1,10 @@
 import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
+import time
+
+
+load_dotenv()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -8,6 +14,8 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
 client = mqtt.Client()
+client.username_pw_set(username=os.getenv('MOSQUITTO_USERNAME'),
+                       password=os.getenv("MOSQUITTO_PASSWORD"))
 client.on_connect = on_connect
 client.on_message = on_message
 
